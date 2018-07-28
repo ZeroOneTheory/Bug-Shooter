da1cda1fa7590e723feadbee12aa1a6025989410
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-07-28 11:16:01 -0400

    Player Movement
    In this edit I added the controlls for getting input and moving the player

---
+++ Bug Shooter/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float health = 5.0f;
+    public float speed = 7.0f;
+
+
+
+	// Use this for initialization
+	void Start () { 
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    Vector3 direction = input.normalized;
+
+    Vector3 velocity = speed * direction;
+    Vector3 moveAmount = velocity * Time.deltaTime;
+
+    transform.Translate(moveAmount);
+}
+}
