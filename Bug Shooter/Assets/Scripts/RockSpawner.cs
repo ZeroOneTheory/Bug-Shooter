8b04515e9ddc421e308fe8e26afc372b9fbf2332
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-07-28 15:31:56 -0400

    Rocks!

---
+++ Bug Shooter/Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour {
+
+    public GameObject rockPrefabs;
+
+    Vector3 screenHalfSize;
+    public float secondsBetweenSpawn = 1;
+    float nextSpawnTime;
+
+
+	// Use this for initialization
+	void Start () {
+        screenHalfSize = new Vector3(Camera.main.aspect * Camera.main.orthographicSize,Camera.main.orthographicSize,0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time > nextSpawnTime)
+        {
+            nextSpawnTime = Time.time + secondsBetweenSpawn;
+            Vector3 spawnPosition = new Vector3(Random.Range(-screenHalfSize.x, screenHalfSize.x), -25, screenHalfSize.y);
+            Instantiate(rockPrefabs, spawnPosition, Quaternion.identity);
+
+        }
+	}
+}
