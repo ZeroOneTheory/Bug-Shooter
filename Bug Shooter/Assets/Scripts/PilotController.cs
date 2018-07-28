7601c90e0914aa848c712c492f81bf0cbb26910a
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-07-28 17:23:31 -0400

    Getting Ready to fly

--- Bug Shooter/Assets/Scripts/PilotController.cs
+++ Bug Shooter/Assets/Scripts/PilotController.cs
@@ -11,6 +11,6 @@ public class PilotController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Input.GetAxisRaw("Vertical"),0.0f, Input.GetAxisRaw("Horizontal"));
+        transform.Rotate(Input.GetAxisRaw("Vertical"),0.0f, -Input.GetAxisRaw("Horizontal"));
 	}
 }
