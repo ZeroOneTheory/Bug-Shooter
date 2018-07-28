43fb99b5a181bb446c7d70e81015ed806d9b184d
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-07-28 11:19:39 -0400

    Code Notation

--- Bug Shooter/Assets/PlayerController.cs
+++ Bug Shooter/Assets/PlayerController.cs
@@ -18,12 +18,17 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+    // Gets the input axis from the controller or keyboard
     Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+    // The normalized vector of these two inputs, equals the direction the player is pressing on a joystick or from a WASD keyboard
     Vector3 direction = input.normalized;
 
+    // Velocity is the player speed times the dirction it's facing
     Vector3 velocity = speed * direction;
+    // Move-Amount is equal to the velocity times the framerate of the game
     Vector3 moveAmount = velocity * Time.deltaTime;
-
+    
+    // Applys move amount to the players position
     transform.Translate(moveAmount);
 }
 }
