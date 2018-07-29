876cea034b7b4e4490f3fe099f0f1484eade2662
ZeroOneTheory <36996656+ZeroOneTheory@users.noreply.github.com>
2018-07-29 16:31:21 -0400

    Added Shooting

---
+++ Bug Shooter/Assets/Scripts/GunScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        Shooting();
+	}
+
+    public Transform canonRight;
+    public Transform canonLeft;
+    public GameObject bulletPrefab;
+    public float fireRate = 6.0f;
+    private float waitTilFire = 0.0f;
+    void Shooting()
+    {
+        if (Input.GetKey(KeyCode.U))
+        {
+            if (waitTilFire <= 0)
+            {
+                Instantiate(bulletPrefab, canonRight.transform.position, canonRight.transform.rotation);
+                Instantiate(bulletPrefab, canonLeft.transform.position, canonLeft.transform.rotation);
+                waitTilFire = 1;
+
+            }
+        }
+        waitTilFire -= fireRate * Time.deltaTime;
+    }
+}
---
+++ Bug Shooter/Assets/Scripts/bulletScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour {
+
+    public float bulletForce = 4000.0f;
+
+    private void Awake()
+    {
+        gameObject.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * bulletForce);
+    }
+    private void FixedUpdate()
+    {
+        BulletLife();
+    }
+
+    //public GameObject bulletHolePrefab;
+    public float distanceFromWall = 0.15f;
+    void BulletLife()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 1000.0f))
+        {
+            if(hit.distance < 3)
+            {
+                Destroy(gameObject);
+            }
+        }
+        Destroy(gameObject, 4.0f);
+    }
+}
